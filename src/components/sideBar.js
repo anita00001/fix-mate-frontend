fe356067e067063c0875a952b634cf3e6b38acdb
moFuhidy <mo7ammed9290@gmail.com>
2023-10-31 13:42:00 +0300

    Create css file for sidebar styling

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -1,4 +1,14 @@
+import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+
 const Sidebar = () => (
-  <div>sideBar</div>
+  <>
+    <button
+      type="button"
+      className="fixed top-5 left-5 text-base z-30"
+    >
+      <HiOutlineMenuAlt4 className="text-2xl" />
+    </button>
+    <div className="sideBar flex h-screen flex-col absolute justify-between border-r bg-white" />
+  </>
 );
 export default Sidebar;
