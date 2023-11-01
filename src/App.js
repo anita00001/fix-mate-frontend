e2d2377e1b295c42c7eb73dea92adcef233a3469
Anita Sharma <sharma.anita00001@gmail.com>
2023-11-01 13:50:45 +0545

    Expert page - Component: Experts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Sidebar from './components/Sidebar';
+import Experts from './components/Experts';
 
 function App() {
   return (
     <>
       <Sidebar />
+      <Experts />
     </>
   );
 }
---
+++ src/components/Experts.js
@@ -0,0 +1,39 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchExperts } from '../redux/Experts/ExpertsSlice';
+import '../styles/Experts.css';
+
+const Experts = () => {
+  const dispatch = useDispatch();
+  const experts = useSelector((state) => state.experts.experts);
+
+  useEffect(() => {
+    dispatch(fetchExperts());
+  }, [dispatch]);
+
+  return (
+    <div className="expert-page">
+      <h1>Meet our Experts</h1>
+      <ul className="ul">
+        {experts.map((expert) => (
+          <li key={expert.id}>
+            {'Name: '}
+            {expert.first_name}
+            {' '}
+            {expert.last_name}
+            <br />
+            {'Specialization: '}
+            {expert.specialization_id }
+            <br />
+            {'Status: '}
+            {expert.status ? 'Available' : 'Not available, please check later'}
+            <br />
+            <button type="submit" className="detail-btn">Details</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Experts;
