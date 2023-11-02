1967f9bb5a996ca2b24922ab9f2aa6425b13e605
moFuhidy <mo7ammed9290@gmail.com>
2023-11-02 13:31:45 +0300

    Display the reservations data

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,53 +1,111 @@
 import DataTable from 'react-data-table-component';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../redux/Reservations/reservationsSlice';
 import Sidebar from './Sidebar';
 
-const columns = [
-  {
-    name: 'id',
-    selector: (row) => row.id,
-  }, {
-    name: 'User Name',
-    selector: (row) => row.name,
-  }, {
-    name: 'Expert Name',
-    selector: (row) => row.fullName,
-  }, {
-    name: 'Specialization',
-    selector: (row) => row.title,
-  }, {
-    name: 'Reservation Date',
-    selector: (row) => row.title,
-  }, {
-    name: 'Reserve date',
-    selector: (row) => row.title,
-  },
+// const column = [
+//   {
+//     name: 'id',
+//     selector: (row) => row.id,
+//   }, {
+//     name: 'User Name',
+//     selector: (row) => row.name,
+//   }, {
+//     name: 'Expert Name',
+//     selector: (row) => row.fullName,
+//   }, {
+//     name: 'Specialization',
+//     selector: (row) => row.title,
+//   }, {
+//     name: 'Reservation Date',
+//     selector: (row) => row.title,
+//   }, {
+//     name: 'Reserve date',
+//     selector: (row) => row.title,
+//   },
 
-];
+// ];
 
-const data = [
-  {
-    id: 1,
-    title: 'Beetlejuice',
-    year: '1988',
-  },
-  {
-    id: 2,
-    title: 'Ghostbusters',
-    year: '1984',
-  },
-];
-const Reservations = () => (
-  <>
-    <Sidebar />
-    <div className="container">
-      <div className="page">
-        <h1>My Reservations</h1>
-        <DataTable
-          columns={columns}
-          data={data}
-        />
+const Reservations = () => {
+  const dispatch = useDispatch();
+  const { reservations, loading, error } = useSelector((state) => state.reservations);
+
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, [dispatch]);
+
+  const formattedData = reservations.map((reservation) => ({
+    id: reservation.id,
+    expert: `${reservation.expert.first_name} ${reservation.expert.last_name}`,
+    specialization: reservation.expert.specialization.name,
+    expertEmail: reservation.expert.email,
+    city: reservation.city,
+    date: reservation.date,
+    user: reservation.user.name,
+  }));
+
+  const columns = [
+    {
+      name: 'id',
+      selector: (row) => row.id,
+    },
+    {
+      name: 'Expert Name',
+      selector: (row) => row.expert,
+    },
+    {
+      name: 'Specialization',
+      selector: (row) => row.specialization,
+    }, {
+      name: 'Expert Email',
+      selector: (row) => row.expertEmail,
+    },
+    {
+      name: 'City',
+      selector: (row) => row.city,
+    },
+    {
+      name: 'Reservation Date',
+      selector: (row) => row.date,
+      sortable: true,
+    },
+    {
+      name: 'User Name',
+      selector: (row) => row.user,
+    },
+  ];
+
+  if (loading === true) {
+    return (
+      <div className=" h-screen w-full flex justify-center items-center">
+        Loading your reservations...
       </div>
-    </div>
-  </>
-);
+    );
+  }
+
+  if (error) {
+    <div className="container">{error}</div>;
+  }
+
+  return (
+    <>
+      <Sidebar />
+      <div className="container">
+        <div className="page">
+          <h1>My Reservations</h1>
+          <p className="text mb-10">
+            These are the your reservations that you have made.
+          </p>
+          <DataTable
+            columns={columns}
+            data={formattedData}
+            pagination
+            responsive
+          />
+        </div>
+      </div>
+    </>
+  );
+};
 export default Reservations;
