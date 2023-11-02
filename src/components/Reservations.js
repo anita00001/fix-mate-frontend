82446ed5f97a1c0eb9fec368ba7c2b98b56e4fc3
moFuhidy <mo7ammed9290@gmail.com>
2023-11-02 13:03:17 +0300

    Install datatdabe component

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,10 +1,52 @@
+import DataTable from 'react-data-table-component';
 import Sidebar from './Sidebar';
 
+const columns = [
+  {
+    name: 'id',
+    selector: (row) => row.id,
+  }, {
+    name: 'User Name',
+    selector: (row) => row.name,
+  }, {
+    name: 'Expert Name',
+    selector: (row) => row.fullName,
+  }, {
+    name: 'Specialization',
+    selector: (row) => row.title,
+  }, {
+    name: 'Reservation Date',
+    selector: (row) => row.title,
+  }, {
+    name: 'Reserve date',
+    selector: (row) => row.title,
+  },
+
+];
+
+const data = [
+  {
+    id: 1,
+    title: 'Beetlejuice',
+    year: '1988',
+  },
+  {
+    id: 2,
+    title: 'Ghostbusters',
+    year: '1984',
+  },
+];
 const Reservations = () => (
   <>
     <Sidebar />
-    <div className="page">
-      <h1>My Reservations</h1>
+    <div className="container">
+      <div className="page">
+        <h1>My Reservations</h1>
+        <DataTable
+          columns={columns}
+          data={data}
+        />
+      </div>
     </div>
   </>
 );
