d1a3c50b990b1ff6213525a4ef8279e0b1fc1e03
moFuhidy <mo7ammed9290@gmail.com>
2023-11-02 13:31:28 +0300

    Add the reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import expertsSlice from './Experts/expertsSlice';
+import reservationsSlice from './Reservations/reservationsSlice';
 import signupSlice from './signup/signupSlice';
 import loginSlice from './login/loginSlice';
 
@@ -9,6 +10,7 @@ const store = configureStore({
     signup: signupSlice,
     login: loginSlice,
     experts: expertsSlice,
+    reservations: reservationsSlice,
   },
   middleware: [thunk],
 });
