ed139730373fa039bc95328b7892197a36652a95
Theodoraldo <theodoraldo@gmail.com>
2023-11-02 05:23:27 +0000

    Modify file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Sidebar from './components/Sidebar';
 import Experts from './components/Experts';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/loginForm';
@@ -19,8 +18,7 @@ const CheckAuthentication = ({ element }) => {
 
 function App() {
   return (
-    <div className="flex w-full overflow-x-hidden">
-      <Sidebar />
+    <div>
       <Routes>
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
