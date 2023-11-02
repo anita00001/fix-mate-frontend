51c662b41defb6b53b6b83ed9919b02a850d3370
moFuhidy <mo7ammed9290@gmail.com>
2023-11-02 11:55:48 +0300

    Fix linters

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -24,6 +24,7 @@ const Sidebar = () => {
         type="button"
         className="closeSidebar sidebarBtn"
         onClick={displayHideNavbar}
+        aria-label={navClose ? 'Open Sidebar' : 'Close Sidebar'}
       >
         <HiOutlineMenuAlt4 className="text-2xl" />
       </button>
@@ -35,6 +36,7 @@ const Sidebar = () => {
           type="button"
           className="closeSidebar sidebarBtn"
           onClick={displayHideNavbar}
+          aria-label={navClose ? 'Close Sidebar' : 'Open Sidebar'}
         >
           <AiOutlineClose className="text-2xl" />
         </button>
