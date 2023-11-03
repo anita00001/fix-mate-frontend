a3dceefdb7c604c7518afbf39fd9ea0d3799a58f
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-03 18:03:28 +0530

    Create Details of Experts that shown of details

--- src/components/ExpertDetails.jsx
+++ src/components/ExpertDetails.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchExperts } from '../redux/Experts/expertsSlice';
+import Sidebar from './Sidebar';
 
-const ExpertDetails = () => (
-  <div>ExpertDetails</div>
-);
+const ExpertDetails = () => {
+  const dispatch = useDispatch();
+  const experts = useSelector((state) => state.experts.experts);
+
+  useEffect(() => {
+    dispatch(fetchExperts());
+  }, [dispatch]);
+
+  const renderStatus = (expert) => {
+    if (expert.status) {
+      return <p style={{ color: 'green' }}>Active</p>;
+    }
+    return <p style={{ color: 'red' }}>Inactive</p>;
+  };
+
+  return (
+    <>
+      <Sidebar />
+      <div className="flex items-center justify-center">
+        {experts.map((expert) => (
+          <div key={expert.id}>
+            <p>{expert.first_name}</p>
+            <p>{expert.last_name}</p>
+            <p>{expert.email}</p>
+            <p>{expert.address}</p>
+            <p>{expert.experience}</p>
+            {renderStatus(expert)}
+            <p>{expert.fee}</p>
+            <p>{expert.name}</p>
+            <p>{expert.description}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default ExpertDetails;
