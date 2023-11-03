4a8ca4cfbdecd99e1697eaf2930eea66b6c6692d
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-03 22:27:02 +0530

    Refator Of Navigation files with folder

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,9 @@
+const navLinks = [
+  { path: '/', name: 'Experts' },
+  { path: '/reservations', name: 'My Reservations' },
+  { path: '/', name: 'Reserve an Expert' },
+  { path: '/experts', name: 'Create an Expert' },
+  { path: '/delete', name: 'Delete an Expert' },
+];
+
+export default navLinks;
