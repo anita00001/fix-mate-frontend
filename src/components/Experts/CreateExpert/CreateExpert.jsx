315e21fe788f46f86b170057fbfb2f21580115e4
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-03 22:28:34 +0530

    Create Folder and refactor to 2 files

---
+++ src/components/Experts/CreateExpert/CreateExpert.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Sidebar from '../../Navigation/Sidebar';
+import '../../../styles/Experts.css';
+import CreateExpertForm from './CreateExpertForm';
+
+const CreateExpert = () => {
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    address: '',
+    email: '',
+    experience: '',
+    fee: '',
+    status: true,
+    specialization_id: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: name === 'fee' || name === 'experience' ? Math.max(0, parseFloat(value)) : value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://127.0.0.1:4000/api/v1/experts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        setFormData({
+          first_name: '',
+          last_name: '',
+          address: '',
+          email: '',
+          experience: '',
+          fee: '',
+          status: true,
+          specialization_id: '',
+        });
+      } else {
+        throw new Error('Something went wrong');
+      }
+    } catch (error) {
+      throw new Error('Error occurred while sending data:');
+    }
+  };
+
+  return (
+    <section className="create-expert-page">
+      <Sidebar />
+      <h1>Create an Expert</h1>
+      <CreateExpertForm
+        formData={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
+    </section>
+  );
+};
+
+export default CreateExpert;
