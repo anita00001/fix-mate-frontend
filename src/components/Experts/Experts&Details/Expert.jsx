72c0b43929089fa9259b1dfd373faf715447d9be
moFuhidy <mo7ammed9290@gmail.com>
2023-11-04 16:46:15 +0300

    Style the slider

--- src/components/Experts/Experts&Details/Expert.jsx
+++ src/components/Experts/Experts&Details/Expert.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 import { fetchExperts } from '../../../redux/Experts/expertsSlice';
 import Sidebar from '../../Navigation/Sidebar';
 import '../../../styles/Experts.css';
+import '../../../styles/swiperCustom.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -31,8 +32,6 @@ const Experts = () => {
           navigation
           pagination={{ clickable: true }}
           scrollbar={{ draggable: true }}
-          onSwiper={(swiper) => console.log(swiper)}
-          onSlideChange={() => console.log('slide change')}
           breakpoints={{
             640: {
               slidesPerView: 2,
@@ -45,28 +44,27 @@ const Experts = () => {
           {experts.map((expert) => (
             <SwiperSlide
               key={expert.id}
-              className="slide z-10 rounded hover:bg-green-200"
+              className="slide z-10 rounded hover:bg-green-100"
             >
-              <Link to={`/experts/${expert.id}`}>
-                <div className="mx-auto mt-20 flex w-[70%] flex-col items-center text-center ">
+              <Link to={`/experts/${expert.id}`} className="w-3/6">
+                <div className="mx-auto mt-20 flex  flex-col items-center text-center ">
                   <img alt={expert.first_name} src={expert.image} />
-                  <br />
+                  <span className="font-thin text-slate-500">
+                    ............................
+                  </span>
                   <span className="font-semibold">
-                    {expert.first_name}
-                    {expert.last_name}
+                    {`${expert.first_name} ${expert.last_name}`}
                   </span>
                   <br />
                   <span className="font-light">
                     Specialization:
                     <span className="font-light text-slate-400">
-                      {expert.name}
+                      {` ${expert.name}`}
                     </span>
                   </span>
                   <br />
                   {'Status: '}
-                  {expert.status
-                    ? 'Available'
-                    : 'Not available, please check later'}
+                  {expert.status ? 'Available' : 'Not available'}
                   <br />
                 </div>
               </Link>
