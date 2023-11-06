edeba75479a989cd32db385c6fe4cf81e500004f
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-11-06 23:11:42 +0530

    Fix back Button

--- src/components/Experts/Experts&Details/ExpertDetails.jsx
+++ src/components/Experts/Experts&Details/ExpertDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams, useNavigate } from 'react-router-dom';
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { useParams, useNavigate, Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { fetchExperts } from '../../../redux/Experts/expertsSlice';
 import Sidebar from '../../Navigation/Sidebar';
@@ -76,19 +75,23 @@ const ExpertDetails = () => {
               </div>
             </div>
           )}
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.9 }}
-            type="button"
-            className="hover:g-primary mt-4 rounded bg-primary px-4 py-2 text-white md:mr-64 md:mt-10 md:self-end md:px-8 md:py-4 md:text-xl"
-          >
-            Reserve
-          </motion.button>
+          <div className="flex justify-end md:ml-[30rem]">
+            <Link to="/new_reservation">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                type="button"
+                className="hover:g-primary mt-4 rounded bg-primary px-4 py-2 text-white md:mt-10 md:self-end md:px-8 md:py-4 md:text-xl"
+              >
+                Reserve
+              </motion.button>
+            </Link>
+          </div>
           <motion.div
             whileHover={{ scale: 1.1 }}
             whileTap={{ scale: 0.9 }}
             onClick={() => navigate(-1)}
-            className="hover:g-primary mt-4 rounded bg-primary px-4 py-2 text-white md:mt-10 md:self-start md:px-8 md:py-4 md:text-xl"
+            className="hover:bg-primary cursor-pointer mt-4 rounded md:-ml-48 bg-primary px-4 py-2 text-white md:mt-10 md:self-start md:px-8 md:py-4 md:text-xl"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
