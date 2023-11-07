87a84e8c06414e529b4f0de4a4c2e5ee5c5fd99a
moFuhidy <mo7ammed9290@gmail.com>
2023-11-07 19:24:57 +0300

    Fix direction

--- src/redux/logout/logoutSlice.js
+++ src/redux/logout/logoutSlice.js
@@ -14,7 +14,7 @@ export const logoutDetails = createAsyncThunk('authentication/logout', async () 
 
     if (response.status === 200) {
       sessionStorage.removeItem('userPassport');
-      window.location.href = '/authenticate';
+      window.location.replace('https://fixmate.netlify.app/authenticate');
     }
   } catch (error) {
     throw new Error(error.message);
